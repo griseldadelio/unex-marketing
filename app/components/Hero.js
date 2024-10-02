89cd800a0fea38f628ca61550cdf1807c28aade0
griselda de lio <griseldadelio@hotmail.com>
2024-10-02 07:59:28 -0300

    ajuste boton hero

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 import Image from 'next/image';
-import Link from 'next/Link';
+import Link from 'next/link';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
 import { Row, Col } from 'react-bootstrap';
